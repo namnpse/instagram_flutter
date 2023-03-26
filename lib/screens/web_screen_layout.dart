01280edf448a63c987a22819510bac8968ec0161
namnpse <nam444479@gmail.com>
2023-03-27 00:38:10 +0700

    added the web layout

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,13 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class WebScreenLayout extends StatelessWidget {
+import '../utils/colors.dart';
+import '../utils/common.dart';
+
+class WebScreenLayout extends StatefulWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
+  State<WebScreenLayout> createState() => _WebScreenLayoutState();
+}
+
+class _WebScreenLayoutState extends State<WebScreenLayout> {
+  late PageController pageController; // for tabs animation
+  int _page = 0; // for tabs animation
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  navigatePage(int p) {
+    //Animating Page
+    pageController.jumpToPage(p);
+    setState(() {
+      _page = p;
+    });
+  }
+
+  void navigationTapped(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is web!'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        centerTitle: false,
+        title: SvgPicture.asset(
+          'assets/ic_instagram.svg',
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => navigatePage(0),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => navigatePage(1),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.add_a_photo,
+              color: _page == 2 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => navigatePage(2),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: _page == 3 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => navigatePage(3),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.person,
+              color: _page == 4 ? primaryColor : secondaryColor,
+            ),
+            onPressed: () => navigatePage(4),
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: navigatePage,
+        children: homeScreenItems,
       ),
     );
   }
