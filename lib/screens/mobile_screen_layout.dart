258ca4e4f24663ae083cecb19a402f58c5737d52
namnpse <nam444479@gmail.com>
2023-04-09 21:20:26 +0700

    set bottom navigation bar item to center

--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 import '../providers/user_provider.dart';
 import '../utils/colors.dart';
 import '../utils/common.dart';
+import '../widgets/lazy_indexed_stack.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -33,17 +34,19 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
 
   void navigationTapped(int page) {
     //Animating Page
-    _pageController.jumpToPage(page);
+    // _pageController.jumpToPage(page);
+    onPageChanged(page);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: onPageChanged,
-        children: homeScreenItems,
-      ),
+      // body: PageView(
+      //   controller: _pageController,
+      //   onPageChanged: onPageChanged,
+      //   children: homeScreenItems,
+      // ),
+      body: _tabViews(),
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
         centerTitle: false,
@@ -66,39 +69,54 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
         backgroundColor: mobileBackgroundColor,
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: (_page == 0) ? primaryColor : secondaryColor,
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Icon(
+                Icons.home,
+                color: (_page == 0) ? primaryColor : secondaryColor,
+              ),
             ),
             label: '',
             backgroundColor: primaryColor,
           ),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.search,
-                color: (_page == 1) ? primaryColor : secondaryColor,
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Icon(
+                  Icons.search,
+                  color: (_page == 1) ? primaryColor : secondaryColor,
+                ),
               ),
               label: '',
               backgroundColor: primaryColor),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.add_circle,
-                color: (_page == 2) ? primaryColor : secondaryColor,
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Icon(
+                  Icons.add_circle,
+                  color: (_page == 2) ? primaryColor : secondaryColor,
+                ),
               ),
               label: '',
               backgroundColor: primaryColor),
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.favorite,
-              color: (_page == 3) ? primaryColor : secondaryColor,
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Icon(
+                Icons.favorite,
+                color: (_page == 3) ? primaryColor : secondaryColor,
+              ),
             ),
             label: '',
             backgroundColor: primaryColor,
           ),
           BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person,
-              color: (_page == 4) ? primaryColor : secondaryColor,
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Icon(
+                Icons.person,
+                color: (_page == 4) ? primaryColor : secondaryColor,
+              ),
             ),
             label: '',
             backgroundColor: primaryColor,
@@ -107,6 +125,60 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
         onTap: navigationTapped,
         currentIndex: _page,
       ),
+      // bottomNavigationBar: Container(
+      //   height: 50,
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       Expanded(
+      //         child: Container(
+      //           alignment: Alignment.center,
+      //           child: Icon(
+      //             Icons.person,
+      //             color: (_page == 1) ? primaryColor : secondaryColor,
+      //           ),
+      //         ),
+      //       ),
+      //       Expanded(
+      //         child: Container(
+      //           alignment: Alignment.center,
+      //           child: Icon(
+      //             Icons.person,
+      //             color: (_page == 2) ? primaryColor : secondaryColor,
+      //           ),
+      //         ),
+      //       ),
+      //       Expanded(
+      //         child: Container(
+      //           alignment: Alignment.center,
+      //           child: Icon(
+      //             Icons.person,
+      //             color: (_page == 3) ? primaryColor : secondaryColor,
+      //           ),
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // ),
+    );
+  }
+
+  Widget _tabViews() {
+    List<Widget> tabs = homeScreenItems;
+    // tabs.add(HomePage());
+    // tabs.add(LeadListRoute.route);
+    // tabs.add(BlocProvider<ListLeadBloc>(
+    //   create: (_) => ListLeadBloc(
+    //     getListOpportunityUseCase:
+    //     GetIt.instance.get<GetListOpportunityUseCase>(),
+    //     getFiltersUseCase: GetIt.instance.get<GetFiltersUseCase>(),
+    //   ),
+    //   child: CustomerListPage(),
+    // ));
+    // tabs.add(MenuPage());
+    return LazyIndexedStack(
+      index: _page,
+      children: homeScreenItems,
     );
   }
 
